f63c4b6d10af5424d95170554374135957c30ddc
petervanderhook <petervanderhook@gmail.com>
2021-12-10 13:27:15 -0800

    Removed redundant code and un-used functions from scripts.js. Added color-scheme for light/dark mode into readme.md. Added comments to help explain functions

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,34 +1,22 @@
 
-console.log('I run')
+
 modetoggle = document.querySelector("input.toggle")
 
+/* Theme switcher. Oh my god the other functions took me 30 seconds and this took me 1.5 hours */
 modetoggle.addEventListener("click", function(evt) {
-    console.log('im running')
+    /* Checks the current theme by comparing the class name on the body element. Simply replaces the class present on the body to change the theme. */
     if(document.body.className == 'light-theme') {
         document.body.classList.replace('light-theme','dark-theme')
     }
     else {
         document.body.classList.replace('dark-theme','light-theme')
-
     }
-    
 })
-
-
-function toDarkMode() {
-    document.querySelector('.toggle:before')
-}
-
-function toLightMode() {
-    x = 0
-}
-
 
 /* Form Section (Outputs to console)*/
 let submit = document.querySelector('#frm-btn')
-
+/* Selects the form and selects each input as a seperate element. Appends the value of the element to a new object and logs the object to the console. */
 submit.addEventListener('click', function(evt) {
-    console.log('Im clicked')
     let message = document.querySelector('#msg')
     let name = document.querySelector('#name')
     let email = document.querySelector('#email')
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # 1620_Exam_fall-21
 Starter code for the CIT 1620 Fall 2021 course
+
+Edited by Peter Vanderhook
+
+
+  /*Light theme colors*/
+  yellow:      #F9C74F;
+  lightCoral:  #EF6E6B;
+  apricot:     #FFD6BA;
+  lightGreen:  #BEE3DB;
+  darkGreen:   #527A78;
+  white:       #FAF9F9;
+  black:       #23262E;
+  blueGray:    #555B6E;
+  romanSilver: #757E95;
+
+  /*Dark theme colors*/
+  d yellow:      #927530;
+  d lightCoral:  #8b403f;
+  d apricot:     #927b6c;
+  d lightGreen:  #68807a;
+  d darkGreen:   #527A78;
+  d white:       #444343;
+  b lack:       #23262E;
+  d blueGray:    #3a3d49;
+  d romanSilver: #515869;
