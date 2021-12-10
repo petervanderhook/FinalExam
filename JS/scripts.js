042475c27b7032895d1591297237e35d7c372035
petervanderhook <petervanderhook@gmail.com>
2021-12-10 12:51:22 -0800

    Added form submission

--- JS/scripts.js
+++ JS/scripts.js
@@ -8,3 +8,20 @@ function change_mode(evt) {
     console.log('im running')
     document.classList.toggle("dark-theme")
 }
+
+
+let submit = document.querySelector('#frm-btn')
+
+submit.addEventListener('click', function(evt) {
+    console.log('Im clicked')
+    let message = document.querySelector('#msg')
+    let name = document.querySelector('#name')
+    let email = document.querySelector('#email')
+
+    let messageObj = {
+        msgName: name.value,
+        msgEmail: email.value,
+        msgMessage: message.value
+    }
+    console.log(messageObj)
+})
